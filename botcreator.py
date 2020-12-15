74c171e37ca2efd0e8d0ac1a774dc3320fd27834
timofey <69044246+timofey260@users.noreply.github.com>
2020-12-15 19:31:55 +0300

    Update botcreator.py

--- botcreator.py
+++ botcreator.py
@@ -1,25 +1,27 @@
 filename = input('botname: ')
-file = filename + '.py'
-bot = open(file, "w+", encoding='utf-8')
+filep = filename + '.py'
+bot = open(filep, "w+", encoding='utf-8')
 to = input('token: ')
-bot.write('token = "%0s"' %(to))
-bot.write('# made by botcreator.py\n'
-           'import config\n'
-           'import discord\n'
-           'class MyClient(discord.Client):\n'
-            '    async def on_ready(self):\n'
-            "       print('Logged on as %0s!')\n" %(filename))
-bot.write("    async def on_message(self, message):\n"
-          "        print(str(message.channel) + ':\\n' + str(message.created_at) + ' ' + str(message.author) + ':' + str(message.content))\n"
+bot.write('token = "%0s"' % (to))
+bot.write('# made by botcreator\n'
+          'import discord\n'
+          'class MyClient(discord.Client):\n'
+          '    async def on_ready(self):\n'
+          "       print('Logged on as %0s!')\n"
+          "    async def on_message(self, message):\n"
+          "        print(str(message.author) + ': ' + str(message.content))\n"
           "        if message.author == self.user:\n"
-          "            return\n")
+          "            return\n" % (filename))
 f = 0
+
+
 def menud(pa1, pa2):
     print("[" + str(pa1) + "] " + pa2)
+
 run = True
 while run:
     menud(1, 'ontext')
-    menud(2,'end')
+    menud(2, 'end')
     name = input('>>> ')
     if name == '1':
         ontext = input('user text: ')
@@ -29,13 +31,21 @@ while run:
                       "            await message.channel.send('%1s')\n" % (ontext, text))
         else:
             bot.write("        elif message.content == '%1s':\n"
-                        "            await message.channel.send('%1s')\n" %(ontext, text))
+                      "            await message.channel.send('%1s')\n" % (ontext, text))
 
     elif name == '2':
         bot.write('client = MyClient()\n'
-                     'client.run(token)\n')
+                  'client.run(token)\n')
+        bot.close()
+        with open(filep) as f:
+            line_count = 0
+            for line in f:
+                line_count = line_count + 1
+        config = open('config.txt', 'w+', encoding='utf-8')
+        config.write(str(line_count))
+        config.close()
         run = False
     else:
         print('is not correct!')
     f = 1
-bot.close()
+

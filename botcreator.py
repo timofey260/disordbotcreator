9f9c81f8c1719438eeadfc0a70fb821cec2205a6
timofey260 <69044246+timofey260@users.noreply.github.com>
2020-12-15 13:32:47 +0300

    Update botcreator.py

--- botcreator.py
+++ botcreator.py
@@ -1,9 +1,6 @@
-g = 'bot.py'
-c = 'config.py'
-bot = open(g, "w", encoding='utf-8')
+bot = open('bot.py', "w", encoding='utf-8')
 to = input('token: ')
-config = open(c, 'w', encoding='utf-8')
-config.write('token = "%0s"' %(to))
+bot.write('token = "%0s"' %(to))
 bot.write('# made by botcreator.py\n'
            'import config\n'
            'import discord\n'
@@ -14,8 +11,7 @@ bot.write("    async def on_message(self, message):\n"
           "        print(str(message.channel) + ':\\n' + str(message.created_at) + ' ' + str(message.author) + ':' + str(message.content))\n"
           "        if message.author == self.user:\n"
           "            return\n")
-bot.write("        if message.content == 'Hi' or message.content == 'HI':\n"
-          "           await message.channel.send('Hi @' + str(message.author) + '!')\n")
+f = 0
 def menud(pa1, pa2):
     print("[" + str(pa1) + "] " + pa2)
 run = True
@@ -26,12 +22,17 @@ while run:
     if name == '1':
         ontext = input('user text: ')
         text = input('bot text: ')
-        bot.write("        elif message.content == '%1s':\n"
-                  "            await message.channel.send('%1s')\n" %(ontext, text))
+        if f == 0:
+            bot.write("        if message.content == '%1s':\n"
+                      "            await message.channel.send('%1s')\n" % (ontext, text))
+        else:
+            bot.write("        elif message.content == '%1s':\n"
+                        "            await message.channel.send('%1s')\n" %(ontext, text))
 
-    if name == '2':
+    elif name == '2':
         bot.write('client = MyClient()\n'
-                     'client.run(config.token)\n')
+                     'client.run(token)\n')
         run = False
     else:
         print('is not correct!')
+    f = 1
